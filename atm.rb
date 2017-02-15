76da95cb8094fbea50e582356540f18751bd19ec
Hannah <thompson.h@gmail.com>
2017-02-15 15:12:23 +1100

    add ATM.rb

---
+++ atm.rb
@@ -0,0 +1,43 @@
+balance = 100
+amount = 20
+
+def withdraw(balance, deduction)
+  if balance >= deduction
+    balance -= deduction
+  else
+    puts "You don't have enough money to do that :("
+  end
+  display(balance)
+end
+
+def deposit(balance, deposit)
+  balance += deposit
+  display(balance)
+end
+
+def display(balance)
+  puts "Balance: "+balance.to_s
+  return(balance)
+end
+
+def action(balance)
+  puts "For deposit enter 'D', for withdrawal enter 'W'"
+  action = gets.chomp
+  if action == 'D' || action == 'd'
+    puts "How much do you want to deposit?"
+    amount = gets.chomp.to_i
+    deposit(balance, amount)
+  elsif action == 'W' || action == 'w'
+    puts "How much do you want to withdraw?"
+    amount = gets.chomp.to_i
+    withdraw(balance, amount)
+  else
+    puts "Error, please enter 'D' or 'W'"
+    display(balance)
+  return balance
+  end
+end
+
+10.times do
+  balance = action(balance)
+end
